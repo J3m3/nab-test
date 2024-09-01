6edf2daf947c5e6fc68ebd065eb49d7bb202bc48
Jesung Yang <y.j3ms.n@gmail.com>
2024-09-01 21:53:44 +0900

    add bunch of scripts

---
+++ src/run-Oz.sh
@@ -0,0 +1,6 @@
+./simple-build-ir-nab_r-544.sh
+
+./asm_gen_non-mod-Oz.sh
+./asm_gen_mod-Oz.sh
+
+./compare_s.sh > result-Oz
---
+++ src/run.sh
@@ -0,0 +1,6 @@
+./simple-build-ir-nab_r-544.sh
+
+./asm_gen_non-mod.sh
+./asm_gen_mod.sh
+
+./compare_s.sh > result-O3
